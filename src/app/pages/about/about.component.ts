6290dc954cf8b1a51ee338768a4fff6b670f098b
Gastondnc <gastondnc@icloud.com>
2024-01-25 17:53:37 +0100

    Revert "feat: add text to speach about"
    This reverts commit f8024998e386e6209086ca75d8c62cfcc834771e.

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent{
+export class AboutComponent {
 
   public arr: string[] = [
     "En un rincón olvidado del universo, una estrella brillaba con intensidad, iluminando el vacío cósmico que la rodeaba. Su luz viajaba a través del espacio, contando historias de mundos lejanos y secretos perdidos.",
@@ -31,14 +31,12 @@ export class AboutComponent{
   public randomIndex: number = 0;
   public message: string = '';
   public fraseArr: string[] = [];
-  private typpingStyle: "word" | "letter" = "letter";
-  private velocity: number = 47;
-
+  private typpingStyle: "word"|"letter" = "letter";
+  private velocity: number = 50;
 
   constructor() {
     this.setIndex()
   }
-
 
   setIndex() {
     this.randomIndex = Math.floor(Math.random() * this.arr.length);
@@ -49,53 +47,32 @@ export class AboutComponent{
   getText() {
     const frase: string = this.arr[this.randomIndex];
     console.log('FRASE', frase);
-    if (this.typpingStyle === 'letter') {
+    if(this.typpingStyle === 'letter'){
       this.fraseArr = frase.split('');
-    } else {
+    }else{
       this.fraseArr = frase.split(' ');
     }
     console.log(this.fraseArr)
     this.buildText(0)
-    this.speak(frase)
   }
 
   buildText(index: number) {
-
-    if (index < this.fraseArr.length) {
-      if (this.typpingStyle === 'letter') {
-        this.message = this.message + this.fraseArr[index];
-      } else {
+    if(index < this.fraseArr.length){
+      if(this.typpingStyle === 'letter'){
+        this.message = this.message +  this.fraseArr[index];
+      }else{
         this.message = this.message + " " + this.fraseArr[index];
       }
-      setTimeout(() => {
+      setTimeout( () => {
         this.buildText(index + 1)
       }, this.velocity)
-    } else {
+    }else{
       console.log(this.message.length, this.fraseArr.length)
-
     }
-
-  }
-
-  speak(message: string) {
-    const synth = window.speechSynthesis
-    const msg = new SpeechSynthesisUtterance(message);
-    msg.voice = synth.getVoices()[0];
-    msg.volume = 1;
-    msg.pitch = 1 ;
-    msg.rate = 1;
-    synth.speak(msg);
   }
 
 
 }
-
-
-/**
- * var msg = new SpeechSynthesisUtterance();
-msg.text = "Hello World";
-window.speechSynthesis.speak(msg);
- */
 
 
 
